0995059d2cab793ced5784cad06cbec7b1562ff1
tilmitt11191 <tilmitt11@gmail.com>
2017-03-27 13:10:28 +0900

    ubuntuMain 2017年  3月 27日 月曜日 13:10:27 JST

---
+++ bin/install_cytoscape.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+VERSION="3.4.0"
+_VERSION=${VERSION//./_} #3.4.0 => 3_4_0
+
+PWD=`sudo pwd`
+cd `dirname $0`
+
+#wget -P ../tmp/ http://chianti.ucsd.edu/cytoscape-$VERSION/Cytoscape_$_VERSION_unix.sh 2>&1 /dev/null
+sudo bash ../tmp/Cytoscape_"$_VERSION"_unix.sh
+
+cd $PWD
+
+exit 0
